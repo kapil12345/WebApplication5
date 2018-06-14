dc928662affd0a83adc3e5e2ed7ad8f0f79e3208
Kapilkumar Bansal <kapilkumar_b@HCL.COM>
2018-06-14 13:27:02 +0000

    chages

--- WebApplication5/Controllers/HomeController.cs
+++ WebApplication5/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication5.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "test.";
 
             return View();
         }
